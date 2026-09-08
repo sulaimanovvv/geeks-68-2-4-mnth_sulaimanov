d63ea37340e857e602a3c7c8e85dc352cfea9f8b
sulaimanovvv <sulaimanoff.imran@yandex.com>
2026-09-08 15:55:42 +0600

    home_work 2 added

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import Post
 
 
 def hello_world(request):
@@ -11,3 +12,9 @@ def my_name(request):
 
 def say_name(request, name):
     return HttpResponse(f"<h2> Hello </h2> <h1>{name}</h1>")
+
+
+def post_list(request):
+    posts = Post.objects.filter(is_published=True)
+    
+    return render(request, "list_posts.html", {"posts": posts})
